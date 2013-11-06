a08030a524e5f5f8ae50bf3324968ae3c5aa0894
Irvyne <thibaud.bardin@gmail.com>
2013-11-06 15:17:35 +0100

    Add User DataFixtures

---
+++ src/YourBooks/UserBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Acme\HelloBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Irvyne\UserBundle\Entity\User;
+
+class LoadUserData implements FixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $userAdmin = new User();
+        $userAdmin->setUsername('admin');
+        $userAdmin->setPlainPassword('admin');
+        $userAdmin->setEmail('fake@thibaud-bardin.com');
+        $userAdmin->addRole('ROLE_ADMIN');
+        $userAdmin->addRole('ROLE_ALLOWED_TO_SWITCH');
+        $userAdmin->setEnabled(true);
+        $manager->persist($userAdmin);
+
+        $user = new User();
+        $user->setUsername('user');
+        $user->setPlainPassword('user');
+        $user->setEmail('fake@thibaud-bardin.com');
+        $user->addRole('ROLE_USER');
+        $user->setEnabled(true);
+        $manager->persist($userAdmin);
+
+        $manager->flush();
+    }
+}
