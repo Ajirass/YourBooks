43829b08ad8682b59b5437a7d2e5862b6b0f2135
Irvyne <thibaud.bardin@gmail.com>
2013-12-10 17:15:11 +0100

    Update assetic.sh script

--- app/scripts/assetic.sh
+++ app/scripts/assetic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-php app/console assets:install &&
-php app/console assetic:dump &&
-php app/console cache:clear
+php app/console cache:clear &&
+php app/console assets:install --symlink &&
+php app/console assetic:dump
